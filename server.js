074d27b923a9e0af78c442956f5e722cc341db0f
khomotsojane <khomotsojane27@gmail.com>
2024-01-09 22:25:06 +0200

    Added payment intent

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 // server.js
 const express = require("express");
-const stripe = require("stripe")("sk_test_51OUsyaJ22JPCn5AcRHbRJXwaI2KdM3jOhA2FEHx27HS7lXwveRvDLxDoTWTAwFMV06fToqOATKLtCjnZeWU1azu400QYaPk8Oq");
+const stripe = require("stripe")("API key from Stripe");
 const app = express();
 const port = 3020;
 
 app.use(express.json());
 
-// Route to handle payment requests
+
 app.post("/process-payment", async (req, res) => {
   try {
     const { amount, currency, token } = req.body;
 
-    // Create a payment intent using the Stripe API
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency,
@@ -20,7 +19,6 @@ app.post("/process-payment", async (req, res) => {
       confirm: true,
     });
 
-    // If the payment was successful, respond with a success message
     res.json({ success: true, message: "Payment processed successfully" });
   } catch (error) {
     console.error("Error processing payment:", error.message);
